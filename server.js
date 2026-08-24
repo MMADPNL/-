375107d4e6799f81232e6f3d814ddd213e5e802d
MMADPNL <mohamad23rezazade@gmail.com>
2026-08-24 14:31:30 +0330

    Update server.js

--- server.js
+++ server.js
@@ -7,130 +7,206 @@ app.use(express.static("public"));
 
 let users = {};
 
-function getUser(id){
-    if(!users[id]){
-        users[id] = {
-            balance: 10000
-        };
-    }
+function persianToEnglish(str){
 
-    return users[id];
+return String(str).replace(/[۰-۹]/g, d =>
+"۰۱۲۳۴۵۶۷۸۹".indexOf(d)
+);
+
 }
 
 
-app.get("/", (req,res)=>{
-    res.send("🚀 DOGS LIMBO SERVER ONLINE");
+function getUser(id){
+
+if(!users[id]){
+
+users[id]={
+balance:10000
+};
+
+}
+
+return users[id];
+
+}
+
+
+
+// تست سرور
+app.get("/",(req,res)=>{
+
+res.send("🚀 DOGS LIMBO SERVER ONLINE");
+
 });
 
 
-// موجودی
+
+
+// گرفتن موجودی
+
 app.get("/balance/:id",(req,res)=>{
 
-    let user = getUser(req.params.id);
 
-    res.json({
-        balance: user.balance
-    });
+let user=getUser(req.params.id);
+
+
+res.json({
+
+balance:user.balance
 
 });
+
+
+});
+
+
+
+
 
 
 // بازی LIMBO
+
 app.post("/limbo",(req,res)=>{
 
-    let id = req.body.id;
-    let bet = Number(req.body.bet);
-    let target = Number(req.body.target);
 
-    let user = getUser(id);
+let id=req.body.id;
 
 
-    if(!bet || !target){
-
-        return res.json({
-            error:"عدد اشتباه"
-        });
-
-    }
+let bet=Number(
+persianToEnglish(req.body.bet)
+);
 
 
-    if(bet > user.balance){
-
-        return res.json({
-            error:"موجودی کافی نیست"
-        });
-
-    }
+let target=Number(
+persianToEnglish(req.body.target)
+);
 
 
-    user.balance -= bet;
+
+let user=getUser(id);
 
 
-    let crash = Number(
-        (Math.random()*9+1).toFixed(2)
-    );
+
+if(!bet || !target){
+
+return res.json({
+
+error:"مقدار اشتباه"
+
+});
+
+}
 
 
-    if(target <= crash){
 
-        let win = Math.floor(
-            bet * target
-        );
+if(bet > user.balance){
 
-        user.balance += win;
+return res.json({
 
+error:"موجودی کافی نیست"
 
-        return res.json({
+});
 
-            win:true,
-
-            prize:win,
-
-            balance:user.balance
-
-        });
-
-    }
+}
 
 
-    res.json({
 
-        win:false,
+user.balance -= bet;
 
-        crash:crash,
 
-        balance:user.balance
 
-    });
+let crash = Number(
+(Math.random()*5+1).toFixed(2)
+);
 
+
+
+if(target <= crash){
+
+
+let win = Math.floor(
+bet * target
+);
+
+
+user.balance += win;
+
+
+
+return res.json({
+
+win:true,
+
+prize:win,
+
+crash:crash,
+
+balance:user.balance
 
 });
 
 
-// شارژ تستی
+}
+
+
+
+res.json({
+
+win:false,
+
+crash:crash,
+
+balance:user.balance
+
+});
+
+});
+
+
+
+
+
+
+
+// شارژ
+
 app.post("/charge",(req,res)=>{
 
-    let user = getUser(req.body.id);
 
-    user.balance += Number(
-        req.body.amount
-    );
+let id=req.body.id;
+
+let amount=Number(
+persianToEnglish(req.body.amount)
+);
 
 
-    res.json({
 
-        balance:user.balance
+let user=getUser(id);
 
-    });
+
+user.balance += amount;
+
+
+
+res.json({
+
+balance:user.balance
 
 });
+
+
+});
+
+
+
+
 
 
 
 app.listen(3000,()=>{
 
-    console.log(
-        "🚀 SERVER RUNNING"
-    );
+console.log(
+"🚀 SERVER RUNNING ON 3000"
+);
 
 });
